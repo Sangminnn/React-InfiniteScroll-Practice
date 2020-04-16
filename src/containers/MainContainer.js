356d1c979c08a06e011b49288d41659025fc2e17
박상민 <psm0102psm@gmail.com>
2020-04-16 23:37:10 +0900

    apply apollo client for local state

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,72 +1,127 @@
 import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { Picture, ImageList } from 'components/Base/MyMain';
+import gql from 'graphql-tag';
+import { useQuery, useMutation } from "@apollo/react-hooks";
 
-function MainContainer() {
-  const [target, setTarget] = useState(null);
-  // const target = useRef(null);
 
-  const [onload, setOnload] = useState(true);
-
-  const [pictures, setPictures] = useState([
-    { image: 'prac4.jpg', title: 'hello'  },
-    { image: 'prac2.jpg', title: 'hello'  },
-    { image: 'prac5.jpg', title: 'hello'  },
-    { image: 'prac4.jpg', title: 'hello'  },
-    { image: 'prac6.jpg', title: 'hello'  },
-    { image: 'prac2.jpg', title: 'hello'  },
-    { image: 'prac1.jpg', title: 'hello'  },
-    { image: 'prac2.jpg', title: 'hello'  },
-    { image: 'prac4.jpg', title: 'hello'  },
-    { image: 'prac5.jpg', title: 'hello'  },
-    { image: 'prac2.jpg', title: 'hello'  },
-    { image: 'prac6.jpg', title: 'hello'  },
-    { image: 'prac3.jpg', title: 'hello'  },
-    { image: 'prac5.jpg', title: 'hello'  },
-    { image: 'prac2.jpg', title: 'hello'  },
-    { image: 'prac1.jpg', title: 'hello'  },
-    { image: 'prac4.jpg', title: 'hello'  },
-    { image: 'prac3.jpg', title: 'hello'  },
-    { image: 'prac2.jpg', title: 'hello'  },
-    { image: 'prac6.jpg', title: 'hello'  },
-    { image: 'prac3.jpg', title: 'hello'  },
-    { image: 'prac5.jpg', title: 'hello'  },
-  ]);
-
-  const addImage = () => {
-    return setPictures([
-      ...pictures,
-      ...pictures
-    ]);
-  };
-
-  useEffect(() => {
-    let observer;
-    if (target) {
-      observer = new IntersectionObserver(onIntersect, { threshold: 1 });
-      observer.observe(target);
-    }
-  }, [target]);
-
-  const onIntersect = ([ entry ]) => {
-    if(entry.isIntersecting) {
-      setOnload(false);
-      addImage();
-      setOnload(true);
+const GET_PICTURES = gql`
+  query GetPictures {
+    pictures @client {
+      image
+      title
     }
   }
+`;
+
+// const LOAD_PICTURES = gql`
+//   mutation LoadPictures {
+//     pictures @client {
+//       image
+//       title
+//     }
+//   }
+// `;
+
+function MainContainer() {
+
+  const { data } = useQuery(GET_PICTURES);
+  
+  // const updateCache = (cache, {data}) => {
+  //   // const existingDatas = cache.readQuery({
+  //   //   query: GET_PICTURES
+  //   // });
+  //   const newthing = data.pictures;
+    
+  //   cache.writeQuery({
+  //     query: GET_PICTURES,
+  //     data: {pictures: [...newthing, ...newthing]}
+  //   })
+  // }
+
+  // const [loadPictures] = useMutation(LOAD_PICTURES, {update: updateCache});
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  // const [target, setTarget] = useState(null);
+  // const target = useRef(null);
+
+  // const [onload, setOnload] = useState(true);
+
+  // const [pictures, setPictures] = useState([
+  //   { image: 'prac4.jpg', title: 'hello'  },
+  //   { image: 'prac2.jpg', title: 'hello'  },
+  //   { image: 'prac5.jpg', title: 'hello'  },
+  //   { image: 'prac4.jpg', title: 'hello'  },
+  //   { image: 'prac6.jpg', title: 'hello'  },
+  //   { image: 'prac2.jpg', title: 'hello'  },
+  //   { image: 'prac1.jpg', title: 'hello'  },
+  //   { image: 'prac2.jpg', title: 'hello'  },
+  //   { image: 'prac4.jpg', title: 'hello'  },
+  //   { image: 'prac5.jpg', title: 'hello'  },
+  //   { image: 'prac2.jpg', title: 'hello'  },
+  //   { image: 'prac6.jpg', title: 'hello'  },
+  //   { image: 'prac3.jpg', title: 'hello'  },
+  //   { image: 'prac5.jpg', title: 'hello'  },
+  //   { image: 'prac2.jpg', title: 'hello'  },
+  //   { image: 'prac1.jpg', title: 'hello'  },
+  //   { image: 'prac4.jpg', title: 'hello'  },
+  //   { image: 'prac3.jpg', title: 'hello'  },
+  //   { image: 'prac2.jpg', title: 'hello'  },
+  //   { image: 'prac6.jpg', title: 'hello'  },
+  //   { image: 'prac3.jpg', title: 'hello'  },
+  //   { image: 'prac5.jpg', title: 'hello'  },
+  // ]);
+
+  // const addImage = () => {
+  //   return setPictures([
+  //     ...pictures,
+  //     ...pictures
+  //   ]);
+  // };
+
+  // useEffect(() => {
+  //   let observer;
+  //   if (target) {
+  //     observer = new IntersectionObserver(onIntersect, { threshold: 1 });
+  //     observer.observe(target);
+  //   }
+  // }, [target]);
+
+  // const onIntersect = ([ entry ]) => {
+  //   if(entry.isIntersecting) {
+  //     setOnload(false);
+  //     loadPictures();
+  //     setOnload(true);
+  //   }
+  // }
   
 
-  const pictureList = pictures.map(
-    picture => (<Picture src={picture.image} title={picture.title}/>)
-  );
+  // const pictureList = pictures.map(
+  //   picture => (<Picture src={picture.image} title={picture.title}/>)
+  // );
+
+  // const pictureList = ({ loading, error, data }) => {
+  //   if(loading) return <h4>loading. ..</h4>;
+  //   if(error) return <h4>Error ...</h4>;
+
+  //   return (
+  //     data.picture.map(
+  //       item => (<Picture src={item.image} title={item.title}/>)
+  //     )
+  //   )
+  // }
 
   return (
     <>
       <ImageList>
-        { pictureList }
+        {data.pictures.map(
+          picture => (<Picture src={picture.image} title={picture.title}/>)
+        )}
       </ImageList>
-      {onload && <Target ref={setTarget}></Target>}
+      {/* {onload && <Target ref={setTarget}></Target>} */}
     </>
   )
 };
--- src/index.js
+++ src/index.js
@@ -2,22 +2,65 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-
 import { BrowserRouter, Route } from 'react-router-dom';
-// import { Provider } from 'react-redux';
+import gql from 'graphql-tag';
+
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { ApolloProvider } from "react-apollo";
 
 import * as serviceWorker from './serviceWorker';
 
-// import configureStore from
+const cache = new InMemoryCache();
 
-// const store = '';
+const typeDefs = gql`
+  type Query {
+    pictures: [Picture]
+  }
+
+  type Picture {
+    __typename: String
+    image: String
+    tilte: String
+  }
+
+  type Mutation {
+    pictures: [Picture]
+  }
+`
+
+// picture의 배열이라는 것을 암시
+
+const client = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    uri: 'http://localhost:4000'
+  }),
+  typeDefs
+});
+
+cache.writeData({
+  data: {
+    // picture: { __typename: "Picture", image: 'prac6.jpg', title: 'hello'  },
+    pictures: [
+      { __typename: "Picture", image: 'prac4.jpg', title: 'hello'  },
+      { __typename: "Picture", image: 'prac2.jpg', title: 'hello'  },
+      { __typename: "Picture", image: 'prac5.jpg', title: 'hello'  },
+      { __typename: "Picture", image: 'prac4.jpg', title: 'hello'  },
+      { __typename: "Picture", image: 'prac6.jpg', title: 'hello'  },
+    ]
+  }
+})
 
 ReactDOM.render(
-  // store 넣어야함
-  // <Provider>
+  <ApolloProvider client={client}>
     <BrowserRouter>
       <Route path="/" component={App} />
-    </BrowserRouter>, document.getElementById('root'));
+    </BrowserRouter>
+  </ApolloProvider>, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
